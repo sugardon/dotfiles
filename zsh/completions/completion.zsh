b49617855d4f4626d5e31bd2eb801b7e9845dca5
sugardon <sugar2don@gmail.com>
2021-02-27 02:34:30 +0900

    update kind completion

--- zsh/completions/completion.zsh
+++ zsh/completions/completion.zsh
@@ -2,6 +2,6 @@
 source <(kubectl completion zsh)
 source <(helm completion zsh)
 source <(skaffold completion zsh)
+source <(kind completion zsh)
 # completion generate
 #stern --completion=zsh > "$HOME/.zsh/completions/_stern"
-#kind completion zsh > "$HOME/.zsh/completions/_kind"
