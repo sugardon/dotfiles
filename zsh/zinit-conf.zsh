2468e9e25d3735a2281f25f5d5c36fdcb8f447f3
sugardon <sugar2don@gmail.com>
2021-03-05 02:07:48 +0900

    update zinit

--- zsh/zinit-conf.zsh
+++ zsh/zinit-conf.zsh
@@ -7,6 +7,7 @@ zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma/fast-syntax-highlighting
 
 ## https://github.com/zsh-users/zsh-history-substring-search
+zinit ice wait lucid \
+    atload"bindkey '^P' history-substring-search-up;\
+           bindkey '^N' history-substring-search-down"
 zinit light zsh-users/zsh-history-substring-search
-bindkey '^P' history-substring-search-up
-bindkey '^N' history-substring-search-down
