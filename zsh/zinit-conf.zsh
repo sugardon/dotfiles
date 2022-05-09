33ac89303139e61c5d9f9e229f023ccc7d71707a
sugardon <sugar2don@gmail.com>
2022-05-09 19:52:59 +0900

    Update completions in zsh

--- zsh/zinit-conf.zsh
+++ zsh/zinit-conf.zsh
@@ -2,7 +2,10 @@
 source $HOME/.zinit/bin/zinit.zsh
 
 zinit snippet OMZ::lib/history.zsh
-zinit snippet OMZ::lib/completion.zsh
+# completions
+zinit light zsh-users/zsh-completions
+zstyle ':completion:*' menu select
+
 zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma/fast-syntax-highlighting
 
