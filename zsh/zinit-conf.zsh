e2afcdf200fb7bae0ddf1d8ae602ae3a3e80448b
sugardon <sugar2don@gmail.com>
2023-07-05 02:05:01 +0900

    Update nvm lazy load

--- zsh/zinit-conf.zsh
+++ zsh/zinit-conf.zsh
@@ -27,9 +27,9 @@ zinit light zsh-users/zsh-history-substring-search
 # https://github.com/lukechilds/zsh-nvm
 export NVM_DIR="$HOME/.nvm"
 export NVM_COMPLETION=true
-export NVM_LAZY_LOAD=true
 export NVM_NO_USE=false
-zinit ice wait lucid
+export NVM_LAZY_LOAD=false
+zinit ice wait"3" lucid
 zinit light lukechilds/zsh-nvm
 
 # `/zsh/completion`
