a93dbbe675c8125148725e2b1d3c9ec4e731013d
sugardon <sugar2don@gmail.com>
2023-07-07 00:10:09 +0900

    Update golang

--- zsh/completions-gen.zsh
+++ zsh/completions-gen.zsh
@@ -3,8 +3,8 @@ mkdir -p $COMP_DIR
 
 kubectl completion zsh > $COMP_DIR/_kubectl
 helm completion zsh > $COMP_DIR/_helm
+kind completion zsh > $COMP_DIR/_kind
 #source <(skaffold completion zsh)
-#source <(kind completion zsh)
 #source <(stern --completion=zsh)
 
 echo "Generate Completions! at COMP_DIR: $COMP_DIR"
