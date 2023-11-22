d1ae3f1bdd4111264f42258a5e91bf03cc1168bf
sugardon <sugar2don@gmail.com>
2023-11-23 01:54:02 +0900

    format

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -2,11 +2,11 @@ local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- delete trailing white spaces except for markdown
 function DeleteTrailingWS()
-	if vim.bo.filetype == "markdown" then
-		return
-	end
-	vim.api.nvim_command([[normal mz]])
-	vim.cmd([[%s/\s\+$//ge]])
-	vim.api.nvim_command([[normal 'z]])
+  if vim.bo.filetype == "markdown" then
+    return
+  end
+  vim.api.nvim_command([[normal mz]])
+  vim.cmd([[%s/\s\+$//ge]])
+  vim.api.nvim_command([[normal 'z]])
 end
 autocmd("BufWritePre", { callback = DeleteTrailingWS })
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
-	-- https://www.lazyvim.org/plugins/colorscheme
-	-- add gruvbox
-	{ "ellisonleao/gruvbox.nvim" },
+  -- https://www.lazyvim.org/plugins/colorscheme
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
 
-	-- Configure LazyVim to load gruvbox
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "gruvbox",
-		},
-	},
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
 }
