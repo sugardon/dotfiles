b5207ed1b84813ea5b4d2f5f0f0b7e4dfa55e3d4
sugardon <sugar2don@gmail.com>
2023-11-30 13:27:01 +0900

    Update keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -20,17 +20,5 @@ keymap("", "<S-Right>", "<C-w>>", defaultOpts)
 keymap("", "<S-Up>", "<C-w>-", defaultOpts)
 keymap("", "<S-Down>", "<C-w>+", defaultOpts)
 
--- buffer
-keymap("", "<leader>b", ":b #<Enter>", defaultOpts)
-
 -- 画面分割
 keymap("", "<leader>s", ":vsplit<Enter>", defaultOpts)
-
--- tab
-keymap("", "<tab>l", "<cmd>bnext<cr>", defaultOpts)
-keymap("", "<tab>h", "<cmd>bprevious<cr>", defaultOpts)
-keymap("", "<tab>t", ":<c-u>tabnew<cr>", defaultOpts)
-keymap("", "<tab>0", ":<c-u>tabfirst<cr>", defaultOpts)
-keymap("", "<tab>4", ":<c-u>tablast<cr>", defaultOpts)
-keymap("", "<tab>$", ":<c-u>tablast<cr>", defaultOpts)
-keymap("", "<tab>c", ":<c-u>tabclose<cr>", defaultOpts)
