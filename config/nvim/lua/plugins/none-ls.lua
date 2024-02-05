fac2e84b73aa339ed02e1588681ee486a3b8139a
sugardon <66208081+sugardon@users.noreply.github.com>
2024-02-06 00:11:49 +0900

    Update (#34)

---
+++ config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "LazyFile",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "codespell" })
+        end,
+      },
+    },
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.diagnostics.codespell,
+      })
+    end,
+  },
+}
