bc9ee45a3f8f234b030322f721a2b462e4fd02e2
sugardon <66208081+sugardon@users.noreply.github.com>
2024-04-08 11:04:14 +0900

    Update lazy (#37)

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -16,6 +16,13 @@ return {
       opts.sources = vim.list_extend(opts.sources or {}, {
         nls.builtins.diagnostics.codespell,
       })
+      nls.setup({
+        -- For `warning: multiple different client offset_encodings detected for buffer, this is not supported yet`
+        -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
+        on_init = function(new_client, _)
+          new_client.offset_encoding = "utf-32"
+        end,
+      })
     end,
   },
 }
